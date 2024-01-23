621faf8da80a71cd220ffb8164e2c2f16c3f0950
Tushar.garggg <tushargarg9868@gmail.com>
2024-01-23 15:09:44 +0530

    add all changes

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,26 +16,26 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/product_31.png";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "this is the boy background";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Web Developer student studying at GGSIPU University. I enjoy creating and developing unique and simplistic user interfaces in creative ways.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
+  "Web developer",
+  "UI/IX",
+  "logo design",
+  "video editor",
+  "Crypto boss",
   "Graphic design",
 ];
 
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "This is my Protfolio Website , i am a web developer and designer.";
 
 const About = () => {
   return (
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/product_35.png";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "this is a boy/kid image ";
 
 /**
  * Project list
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "My second protfolio",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "This is my portfolio Developed by me using React, Javascript, HTML and CSS.with all infromation about myself hope you will like it.",
+    url: "https://tushargargggg.netlify.app/",
   },
   {
-    title: "Web Development for Beginners",
+    title: "landing page for a company",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "ths is a landing page for a chocolate company",
+    url: "https://mokamaid.netlify.app/#",
   },
   {
-    title: "My Resume Site",
+    title: "soulbound confession",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "Created ths site for my hackathone project ",
+    url: "https://soulboundconfessions.netlify.app/",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "restro website",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "Website for a restro.",
+    url: "https://redchillii.netlify.app/",
   },
 ];
 
